47e3c33bfe65fb9b4b785a3cc3fd025abeded62c
NavaneethVijay <NavaneethVijay>
2020-04-07 22:23:33 +0530

    projext id changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,11 @@ module.exports = {
    ** Customize the progress-bar color
    */
   loading: { color: '#fff' },
+  loadingIndicator: {
+    name: 'rotating-plane',
+    color: '#3B8070',
+    background: 'white',
+  },
   /*
    ** Global CSS
    */
@@ -67,7 +72,7 @@ module.exports = {
           apiKey: process.env.FB_API_KEY,
           authDomain: process.env.FB_AUTH_DOMAIN,
           databaseURL: process.env.FB_DATABASE_URL,
-          projectId: process.env.FB_PROJECT_ID,
+          projectId: 'witless-todo',
           storageBucket: process.env.FB_STORAGE_BUCKET,
           messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
           appId: process.env.FB_APP_ID,
