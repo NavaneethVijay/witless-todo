6224f9d8bf4839020de5e19dc0daae9adb9ae05a
NavaneethVijay <NavaneethVijay>
2020-04-08 14:11:58 +0530

    manifest and icon updates

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="layout-main">
     <Loader />
-    <Header v-if="isLoggedIn" class="header" />
+    <Header class="header" />
     <nuxt />
   </div>
 </template>
@@ -24,18 +24,12 @@ export default {
 </script>
 
 <style>
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
+.header {
+  max-width: 1440px;
+  margin: auto;
+  position: sticky;
+  top: 0;
 }
-
 *,
 *:before,
 *:after {
